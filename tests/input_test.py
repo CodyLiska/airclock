1623a3a1f41d2cd45c88473749b314af20f2e735
Cody Liska <codyliska@gmail.com>
2026-07-30 20:19:41 -0700

    Revert invalid pull_up kwarg on RotaryEncoder

--- tests/input_test.py
+++ tests/input_test.py
@@ -1,7 +1,7 @@
 from gpiozero import RotaryEncoder, Button
 from signal import pause
 
-encoder = RotaryEncoder(17, 27, wrap=True, pull_up=True)
+encoder = RotaryEncoder(17, 27, wrap=True)
 encoder.when_rotated_clockwise = lambda: print("Clockwise")
 encoder.when_rotated_counter_clockwise = lambda: print("Counter Clockwise")
 
